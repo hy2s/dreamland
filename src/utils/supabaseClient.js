e821be12485dab19228e8465b061ba5ef5f80080
hy2s <rlagptmd1021@naver.com>
2025-10-03 13:31:04 +0900

    supabase API key 수정

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// const supabaseUrl = 'https://fpnosrvzktinuupjmktg.supabase.co';
-const supabaseUrl = 'https://odrzrzehngxzziahvkbh.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kcnpyemVobmd4enppYWh2a2JoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQzNjAzMzksImV4cCI6MjA2OTkzNjMzOX0.8kVDYNBWyXTDDVzT2kYNKZJT2dED2WSjpgVVq5E1BaM';
+const supabaseUrl = 'https://pmlvbtwayftzusksvwbu.supabase.co';
+
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtbHZidHdheWZ0enVza3N2d2J1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk0MjI5ODYsImV4cCI6MjA3NDk5ODk4Nn0.D7zavW3Q8ZJqYIdKuVcD-kDHIQmZmlacuFyCr-fVsDw';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
